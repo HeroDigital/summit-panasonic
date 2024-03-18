9b249f694db94213688205c970328b8de465def3
Sean McAuliffe <sean.mcauliffe@herodigital.com>
2024-03-18 16:22:03 -0400

    cards > fix mobile layout, eslint --fix for js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,31 +1,29 @@
-import { createOptimizedPicture } from "../../scripts/aem.js";
+import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   // Change to ul, li
-  const ul = document.createElement("ul");
+  const ul = document.createElement('ul');
   [...block.children].forEach((row, index) => {
-    const li = document.createElement("li");
+    const li = document.createElement('li');
     li.id = `list-item-${index + 1}`; // Assigning unique IDs to each li
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector("img"))
-        div.className = "cards-card-image-container";
-      else div.className = "cards-card-body";
+      if (div.children.length === 1 && div.querySelector('img')) div.className = 'cards-card-image-container';
+      else div.className = 'cards-card-body';
     });
     ul.append(li);
   });
-  ul.querySelectorAll("img").forEach((img) => {
+  ul.querySelectorAll('img').forEach((img) => {
     const container = img.parentElement;
-    container.classList.add("cards-card-image-container");
-    const squareOverlay = document.createElement("div");
-    squareOverlay.classList.add("cards-card-image-overlay");
-    squareOverlay.innerHTML =
-      '<div class="cards-card-image-text">' + img.alt + "</div>";
+    container.classList.add('cards-card-image-container');
+    const squareOverlay = document.createElement('div');
+    squareOverlay.classList.add('cards-card-image-overlay');
+    squareOverlay.innerHTML = `<div class="cards-card-image-text">${img.alt}</div>`;
     container.appendChild(squareOverlay);
     container.replaceWith(
-      createOptimizedPicture(img.src, img.alt, false, [{ width: "750" }])
+      createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]),
     );
   });
-  block.textContent = "";
+  block.textContent = '';
   block.append(ul);
 }
